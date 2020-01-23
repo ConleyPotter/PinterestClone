091323e9d142d504296a1b99824f8779daadfde5
Conley Potter <conley.potter@gmail.com>
2020-01-23 14:15:01 -0600

    Connect to Mongo

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = require('./config/keys').mongoURI;
 
-app.get('/', (req, res) => res.send("Hello World"));
+// DB CONNECTION
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 // MIDDLEWARE
 app.use(bodyParser.json());

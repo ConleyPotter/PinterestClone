dea42f1864ee6d613f7065a10b26991e419e5f1e
Conley Potter <conley.potter@gmail.com>
2020-01-23 14:16:52 -0600

    GraphQL groundwork

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
 const db = require('./config/keys').mongoURI;
+
+// all requests coming in to `graphql` will be handled
+// by the expressGraphQL function from the 'express-graphql' library
+app.use(
+  '/graphql',
+  expressGraphQL({
+    graphiql: true
+  })
+);
 
 // DB CONNECTION
 mongoose

14c8f329cc04799459b79d6765ddfe81fa367adf
Conley Potter <conley.potter@gmail.com>
2020-01-30 15:22:01 -0600

    Backend Auth: Graphql backend mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,13 +1,15 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLNonNull } = graphql;
+const {
+  GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID,
+} = graphql;
 const UserType = require('./types/user_type');
 const AuthService = require('../services/auth');
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    newUser: {
+    register: {
       type: UserType,
       args: {
         name: { type: new GraphQLNonNull(GraphQLString) },
@@ -16,6 +18,34 @@ const mutation = new GraphQLObjectType({
       },
       resolve(_, data) {
         return AuthService.register(data);
+      },
+    },
+    logout: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(_, args) {
+        return AuthService.logout(args);
+      },
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(_, args) {
+        return AuthService.login(args);
+      },
+    },
+    verifyUser: {
+      type: UserType,
+      args: {
+        token: { type: GraphQLString },
+      },
+      resolve(_, args) {
+        return AuthService.verifyUser(args);
       },
     },
   },
